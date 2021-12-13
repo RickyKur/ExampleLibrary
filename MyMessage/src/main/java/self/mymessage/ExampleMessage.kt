ba39d083ea77067e97c0d2d2f93b6def96137f4f
ricky.kurniawan <ricky.kurniawan@gdn-commerce.com>
2021-12-13 22:27:16 +0700

    Added functionality

--- MyMessage/src/main/java/self/mymessage/ExampleMessage.kt
+++ MyMessage/src/main/java/self/mymessage/ExampleMessage.kt
@@ -1,7 +1,30 @@
 package self.mymessage
 
-class ExampleMessage() {
-  init {
+import android.content.Context
+import android.widget.Toast
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import java.lang.ref.WeakReference
 
+class ExampleMessage(
+  private val context: Context,
+  private val lifeCycle: Lifecycle
+): DefaultLifecycleObserver {
+
+  private var weakContext: WeakReference<Context>? = WeakReference(null)
+
+  init {
+    weakContext = WeakReference(context)
+  }
+
+  override fun onDestroy(owner: LifecycleOwner) {
+    weakContext = null
+  }
+
+  fun showToastMessage(message: String) {
+    if (lifeCycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
+      Toast.makeText(weakContext?.get(), message, Toast.LENGTH_SHORT).show()
+    }
   }
 }
